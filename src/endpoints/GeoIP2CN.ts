addfc3b4ac33519d686b0defd6bbf09b1e7df663
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2025-05-12 21:13:45 +0000

    fix(GeoIP2CN): remove ending newline for list text

--- src/endpoints/GeoIP2CN.ts
+++ src/endpoints/GeoIP2CN.ts
@@ -1,7 +1,7 @@
 import { Context } from "hono";
 
 export async function geoip2cn(c: Context) {
-    const CNIPCidr = await (await fetch("https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt")).text();
+    const CNIPCidr = (await (await fetch("https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt")).text()).trim();
     switch (c.req.param("format")) {
         case "surge":
             return new Response(CNIPCidr.split("\n").map(l => "IP-CIDR," + l).join("\n"), {
